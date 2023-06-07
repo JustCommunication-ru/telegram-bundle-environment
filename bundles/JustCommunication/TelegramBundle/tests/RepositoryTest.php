6b845cc1b20cbbd033f7446afb73d8e088334872
mihaylo <mihaylo47@gmail.com>
2023-06-08 09:49:54 +1000

    Отмена неправильного пути для тестов, это все не нужно внутри бандла
    This reverts commit 8c423ca6ad13ec8ddf49ea436e2236670a5cb4f3.

--- bundles/JustCommunication/TelegramBundle/tests/RepositoryTest.php
+++ bundles/JustCommunication/TelegramBundle/tests/RepositoryTest.php
@@ -6,33 +6,28 @@ namespace JustCommunication\TelegramBundle\Tests\Unit;
 //ini_set("display_errors", 1); // для development =1 (ниже)
 
 
-use JustCommunication\TelegramBundle\Repository\TelegramEventRepository;
 use JustCommunication\TelegramBundle\Tests\App\TestingKernel;
 use PHPUnit\Framework\TestCase;
 use Psr\Log\LoggerInterface;
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\HttpKernel\HttpKernelBrowser;
-use Symfony\Contracts\Service\Attribute\Required;
 
 
-class RepositoryTest extends KernelTestCase
+class RepositoryTest extends TestCase
 {
-
-    #[Required]
-    public TelegramEventRepository $telegramEventRepository;
 
     function setUp():void{
 
     }
 
-    public function testGetEvents(){
 
-        self::bootKernel();
-        $container = static::getContainer();
-
-        $telegramEventRepository = $container->get(TelegramEventRepository::class);
-        $telegramEventRepository->getEvents(true);
-
+    public static function createClient()
+    {
+        $kernel = new TestingKernel();
+        return new HttpKernelBrowser($kernel);
+    }
+    public function testSomeTest(){
+        $client = static::createClient();
+        $response = $client->request("GET", "/telega");
         //dd($response);
         $this->assertTrue(true);
     }
